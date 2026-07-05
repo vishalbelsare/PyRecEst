0797c650abe74446b6378a716f9de78478f0fa03
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-05 10:23:29 +0200

    Preserve circular uniform range integration

--- src/pyrecest/distributions/hypertorus/hypertoroidal_uniform_distribution.py
+++ src/pyrecest/distributions/hypertorus/hypertoroidal_uniform_distribution.py
@@ -205,7 +205,8 @@ class HypertoroidalUniformDistribution(
             left, right = integration_boundaries
         left = _validate_boundary("left", left, self.dim)
         right = _validate_boundary("right", right, self.dim)
-        _validate_boundary_order(left, right)
+        if self.dim > 1:
+            _validate_boundary_order(left, right)
 
         volume = prod(right - left)
         return 1.0 / (2.0 * pi) ** self.dim * volume
