173c4a6f694084cadbbd8b2a4717e2aeb446d68c
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-07 21:06:33 +0200

    Prepare hypertorus scalar shape patch

--- src/pyrecest/distributions/hypertorus/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Hypertoroidal distribution exports."""
-
-from .low_rank_hypertoroidal_fourier_distribution import (
-    LowRankHypertoroidalFourierDistribution,
-)
-
-__all__ = ("LowRankHypertoroidalFourierDistribution",)
