503065be22dcc503b8664986ac3b43cf1a46b2c0
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-08 18:01:11 +0200

    Add temporal assignment cost regressions

--- tests/utils/test_assignment_cost_matrix_validation.py
+++ tests/utils/test_assignment_cost_matrix_validation.py
@@ -82,11 +82,53 @@ class AssignmentCostMatrixValidationTest(unittest.TestCase):
         pyrecest.backend.__backend_name__ == "jax",  # pylint: disable=no-member
         reason="Not supported on the JAX backend",
     )
+    def test_temporal_cost_matrix_entries_are_rejected(self):
+        temporal_matrices = (
+            np.array([[np.timedelta64(1, "ns")]]),
+            np.array([[np.datetime64("1970-01-01T00:00:00.000000001")]]),
+            np.array([[np.timedelta64(1, "ns")]], dtype=object),
+            np.array(
+                [[np.datetime64("1970-01-01T00:00:00.000000001")]],
+                dtype=object,
+            ),
+        )
+        for solver_name, solver in self._solvers():
+            for matrix in temporal_matrices:
+                with self.subTest(solver=solver_name, dtype=str(matrix.dtype)):
+                    with self.assertRaisesRegex(
+                        ValueError, "cost_matrix must be numeric"
+                    ):
+                        solver(matrix)
+
+    @unittest.skipIf(
+        pyrecest.backend.__backend_name__ == "jax",  # pylint: disable=no-member
+        reason="Not supported on the JAX backend",
+    )
     def test_text_non_assignment_costs_are_rejected(self):
         matrix = np.array([[1.0]])
         invalid_costs = (
             {"row_non_assignment_costs": np.array(["0.5"])},
             {"col_non_assignment_costs": np.array([b"0.5"], dtype=object)},
+        )
+        for kwargs in invalid_costs:
+            with self.subTest(kwargs=tuple(kwargs)):
+                with self.assertRaisesRegex(ValueError, "must be numeric and finite"):
+                    murty_k_best_assignments(matrix, k=1, **kwargs)
+
+    @unittest.skipIf(
+        pyrecest.backend.__backend_name__ == "jax",  # pylint: disable=no-member
+        reason="Not supported on the JAX backend",
+    )
+    def test_temporal_non_assignment_costs_are_rejected(self):
+        matrix = np.array([[1.0]])
+        invalid_costs = (
+            {"row_non_assignment_costs": np.array([np.timedelta64(1, "ns")])},
+            {
+                "col_non_assignment_costs": np.array(
+                    [np.datetime64("1970-01-01T00:00:00.000000001")],
+                    dtype=object,
+                )
+            },
         )
         for kwargs in invalid_costs:
             with self.subTest(kwargs=tuple(kwargs)):
