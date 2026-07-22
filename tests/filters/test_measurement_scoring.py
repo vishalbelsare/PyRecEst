438904e106288de3472e6bfd8f98918e00dfa97a
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-22 04:19:51 +0200

    Test measurement score index ownership

--- tests/filters/test_measurement_scoring.py
+++ tests/filters/test_measurement_scoring.py
@@ -15,3 +15,23 @@ def _score(active_measurement_indices):
 def test_measurement_score_is_active_handles_numpy_index_arrays():
     assert _score(np.array([0, 2])).is_active
     assert not _score(np.array([], dtype=int)).is_active
+
+
+def test_measurement_score_owns_active_measurement_indices():
+    active_measurement_indices = []
+    score = _score(active_measurement_indices)
+
+    active_measurement_indices.append(0)
+
+    assert score.active_measurement_indices == ()
+    assert not score.is_active
+
+
+def test_measurement_score_copies_numpy_index_arrays():
+    active_measurement_indices = np.array([0, 2])
+    score = _score(active_measurement_indices)
+
+    active_measurement_indices[:] = 1
+
+    assert score.active_measurement_indices == (0, 2)
+    assert score.is_active
